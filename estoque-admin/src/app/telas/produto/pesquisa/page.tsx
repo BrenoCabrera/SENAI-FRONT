7444304dd7a3276233bcae4546d22635d91dd687
BrenoCabrera <breninhopac@gmail.com>
2026-04-08 16:31:41 -0300

    atu

--- estoque-admin/src/app/telas/produto/pesquisa/page.tsx
+++ estoque-admin/src/app/telas/produto/pesquisa/page.tsx
@@ -1,5 +1,61 @@
-export default function TelaPesquisaProduto(){
-    return(
-        <h1>TELA DE PESQUISA DE PRODUTOS</h1>
-    )
+"use client";
+import styles from "../../../pesquisa.module.css";
+
+export default function TelaPesquisaProduto() {
+    return (
+        <section className={styles.container}>
+            <h3 className={styles.titulo}>
+                Pesquisa de Produtos
+            </h3>
+
+            <hr className={styles.hr}/>
+
+            
+
+            <div className={styles.cont1}>
+                <label htmlFor="categoria" className={styles.label}>Categoria:</label>
+                <select name="categoria" id="categoria" className={styles.select}>
+                    <option value="canecas">Canecas</option>
+                    <option value="chaleiras">Chaleiras</option>
+                </select>
+
+                <label htmlFor="desc" className={styles.label}>Nome ou Descrição:</label>
+                <input
+                    type="text"
+                    name="desc"
+                    className={styles.input}
+                />
+
+                <button className={styles.button}>
+                    Pesquisar
+                </button>
+            </div>
+
+            <div className={styles.tabela}>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            <th>Categoria</th>
+                            <th>Nome</th>
+                            <th>Quantidade</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>oi</td>
+                            <td>oi</td>
+                            <td>oi</td>
+                        </tr>
+
+                        <tr>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    );
 }
